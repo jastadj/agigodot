b60d72a01b28a58d79b9ac1e5025bc73d9823f8d
John <jastadj@gmail.com>
2024-03-09 06:54:20 -0800

    toggle transparency on/off

--- scenes/agieditor/agieditor.gd
+++ scenes/agieditor/agieditor.gd
@@ -3,18 +3,22 @@ extends Node2D
 @onready var view_list = $CanvasLayer/ui/Control/PanelContainer/VBoxContainer/HBoxContainer/OptionButtonViews
 @onready var loop_list = $CanvasLayer/ui/Control/PanelContainer/VBoxContainer/HBoxContainer/OptionButtonLoop
 @onready var cel_list = $CanvasLayer/ui/Control/PanelContainer/VBoxContainer/HBoxContainer/OptionButtonCel
-@onready var cel_texture_rect = $CanvasLayer/ui/Control/PanelContainer/VBoxContainer/TextureRectCel
+@onready var cel_transparency = $CanvasLayer/ui/Control/PanelContainer/VBoxContainer/HBoxContainer/CheckBoxTransparency
 @onready var playpause = $CanvasLayer/ui/Control/PanelContainer/VBoxContainer/HBoxContainer/ButtonPlayAnimation
+
+@onready var view_sprite = $CanvasLayer/ui/Control/PanelContainer/VBoxContainer/MarginContainerViewSprite/Sprite2DView
+@onready var container_view_sprite = $CanvasLayer/ui/Control/PanelContainer/VBoxContainer/MarginContainerViewSprite
 
 var image
 var image_texture
 var view_scale = 4
+var view_scale_factor = 0.2
 
 func _ready():
 	
 	image = Image.create(64,64, false, Image.FORMAT_RGBA8)
 	image.fill(Color.RED)
-	_update_texture_rect()
+	_update_view_sprite()
 	
 	# testing
 	_on_file_dialog_game_dir_dir_selected("res://testdata/pq1")
@@ -24,17 +28,19 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.pressed:
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				view_scale += 1
-				_update_texture_rect()
+				view_scale -= view_scale * view_scale_factor
+				_update_view_sprite()
 			elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
-				view_scale -= 1
-				_update_texture_rect()
+				view_scale += view_scale * view_scale_factor
+				_update_view_sprite()
 
 
-func _update_texture_rect():
-	image.resize(image.get_width()*view_scale*2, image.get_height()*view_scale, 0)
+func _update_view_sprite():
+	image.resize(image.get_width(), image.get_height(), 0)
 	image_texture = ImageTexture.create_from_image(image)
-	cel_texture_rect.texture = image_texture	
+	view_sprite.texture = image_texture	
+	view_sprite.scale = Vector2(view_scale*2, view_scale)
+	view_sprite.position = Vector2(0,-view_sprite.get_rect().size.y * view_scale)
 
 func _update_view_list():
 	view_list.clear()
@@ -52,7 +58,7 @@ func _update_loop_list():
 	if(view_list.selected < 0 or !System.agi.views[view_list.selected]):
 		cel_list.clear()
 		image.fill(Color.TRANSPARENT)
-		_update_texture_rect()
+		_update_view_sprite()
 		return
 	
 	for i in range(0, System.agi.views[view_list.selected]["loops"].size()):
@@ -92,43 +98,18 @@ func _select_cel(index):
 	cel_list.select(index)
 	
 	var cel = System.agi.views[view_list.selected]["loops"][loop_list.selected]["cels"][index]
-	var pixeldata = PackedByteArray()
 	var curview = System.agi.views[view_list.selected]
 	var curloop = curview["loops"][loop_list.selected]
 	var curcel = curloop["cels"][cel_list.selected]
-	
-	#print("offset: 0x%x" % curview["offset"], " loops:", curview["loops"].size())
-	#print("loop:", loop_list.selected, ", cel:", cel_list.selected, ", w:", curcel["w"], ", h:", curcel["h"], ", t:", curcel["t"] )
-	
-	var cur_x = 0
-	var line_counter = 0
-	for d in cel["d"]:
-		if d != 0:
-			var runlen = d & 0x0f
-			var color = System.agi.color[ (d & 0xf0) >> 4]
-			cur_x += runlen
-			for run in range(0, runlen):
-				pixeldata.append(color.r8)
-				pixeldata.append(color.g8)
-				pixeldata.append(color.b8)
-				pixeldata.append(color.a8)
-		if d == 0:
-			var color = System.agi.color[ cel["t"]]
-			for run in range(0, cel["w"] - cur_x):
-				pixeldata.append(color.r8)
-				pixeldata.append(color.g8)
-				pixeldata.append(color.b8)
-				pixeldata.append(color.a8)
-			cur_x = 0
-			line_counter += 1
-	#print("h:", cel["h"], ", w:", cel["w"], ", pixels:", pixeldata.size())
+
+	var pixeldata = View.cel_data_to_pixels(curcel, cel_transparency.button_pressed)
 	
 	image = Image.create_from_data( cel["w"], cel["h"], false, Image.FORMAT_RGBA8, pixeldata)
 	
 	if cel["m"] & 0x8:
 		if loop_list.selected != (cel["m"] & 0x7):
 			image.flip_x()
-	_update_texture_rect()
+	_update_view_sprite()
 
 func _on_option_button_views_item_selected(index):
 	_select_view(index)
@@ -151,6 +132,3 @@ func _on_timer_animation_timeout():
 	if playpause.button_pressed:
 		_advance_cel()
 
-
-func _on_button_play_animation_toggled(button_pressed):
-	pass # Replace with function body.
